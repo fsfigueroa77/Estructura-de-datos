3b1e6bdbe6ec7fa31fa83fe2ea09b28e902a55a9
fsfigueroa77 <148596739+fsfigueroa77@users.noreply.github.com>
2024-06-20 19:25:45 -0500

    Update recursividad_tabla_multiplicar.cpp

--- recursividad_tabla_multiplicar.cpp
+++ recursividad_tabla_multiplicar.cpp
@@ -5,20 +5,20 @@ int recursividad(int cont, int nume);
 //Cuerpo del programa
 int main(){
     char auxi[10];
-    int Cont = 1;   
+    int Cont = 1; //Contador que determinara las veces que se multiplica el valor ingresado
     int Nume;
     printf("Ingrese un numero para hacer la tabla de multiplicar: ");
     gets(auxi);
     Nume = atoi(auxi);
-    recursividad(Cont, Nume);    
+    recursividad(Cont, Nume);  //Funcion recursiva que calcula e imprime la tabla de multiplicar 
 }
 
-int recursividad(int cont, int nume){         
+int recursividad(int cont, int nume){
     if(cont == 13){
-        nume = 0;
-    }else{        
+        return; 
+    }else{
         printf("%d x %d = %d\n", cont, nume, cont * nume);
-        nume = nume + recursividad(cont + 1, nume);
+        recursividad(cont + 1, nume);
     }
     return nume;
 }
